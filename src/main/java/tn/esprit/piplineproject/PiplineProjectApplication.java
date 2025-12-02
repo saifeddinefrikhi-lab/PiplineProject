f64af0348ee4387ce9d2fda0426622b85feee482
saifeddinefrikhi-lab <saifeddine.frikhi@esprit.tn>
2025-12-02 21:01:41 +0100

    Intial Commit

---
+++ src/main/java/tn/esprit/piplineproject/PiplineProjectApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.piplineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiplineProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiplineProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/piplineproject/PiplineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.piplineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiplineProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
